a695a71000a36a4e4ee52b6c8e3d6a7639efcc8b
Artsiom_Bukhautsou <artyom.bukhavtsov@gmail.com>
2019-04-25 20:42:01 +0300

    sql script has been updated

--- database/script/restful_app.sql
+++ database/script/restful_app.sql
@@ -1,13 +1,12 @@
 create table developers
 (
-  id           serial      not null
+  id            serial      not null
     constraint developers_pk
       primary key,
-  name         varchar(20) not null,
-  age          integer     not null,
+  name          varchar(20) not null,
+  age           integer     not null,
   primary_skill varchar(20) not null
 );
 
 alter table developers
   owner to postgres;
-
