3cece3e2165ad1f86a984138594457b85c76782f
Artsiom_Bukhautsou <artyom.bukhavtsov@gmail.com>
2019-05-04 11:56:48 +0300

    jwt auth with correct exp has been added

--- jwt/handler.go
+++ jwt/handler.go
@@ -2,13 +2,13 @@ package jwt
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/bukhavtsov/restful-app/daos"
 	"github.com/bukhavtsov/restful-app/models"
 	"github.com/dgrijalva/jwt-go"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -16,6 +16,8 @@ const (
 	secretKeyAccess  = "eXamp1eK3yACceS$"
 	secretKeyRefresh = "r3Fr3S4eXamp1eK3y"
 	iss              = "restful-app"
+	refreshTokenName = "refresh_token"
+	accessTokenName  = "access_token"
 )
 
 type jti struct {
@@ -23,7 +25,7 @@ type jti struct {
 	Role string `json:"Role"`
 }
 
-func Parse(tokenString, secretKey string) (*jwt.Token, error) {
+func parse(tokenString, secretKey string) (*jwt.Token, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v\n", token.Header["alg"])
@@ -37,49 +39,48 @@ func Parse(tokenString, secretKey string) (*jwt.Token, error) {
 }
 
 func VerifyMiddleware(endPoint func(w http.ResponseWriter, r *http.Request)) http.Handler {
-	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			if isVerifiedAccess(r) {
-				endPoint(w, r)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if isVerifiedAccess(r) {
+			endPoint(w, r)
+			return
+		} else if isVerifiedRefresh(r) {
+			refresh, err := r.Cookie(refreshTokenName)
+			if err != nil {
+				log.Println(err)
 				return
-			} else if isVerifiedRefresh(r) {
-				refresh, err := r.Cookie("refresh_token")
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				user, err := getUser(refresh.Value, secretKeyRefresh)
-				if err != nil {
-					log.Println(err)
-					w.WriteHeader(http.StatusGone)
-					return
-				}
-				updatedAccess, err := getUpdatedAccess(*user)
-				if err != nil {
-					fmt.Println("access token :", err)
-					return
-				}
-				http.SetCookie(w, &http.Cookie{Name: "access_token", Value: updatedAccess})
-				endPoint(w, r)
-				return
-			} else {
+			}
+			user, err := getUser(refresh.Value, secretKeyRefresh)
+			if err != nil {
+				log.Println(err)
 				w.WriteHeader(http.StatusGone)
 				return
 			}
-		})
+			updatedAccess, err := getUpdatedAccess(*user)
+			if err != nil {
+				fmt.Println("access token :", err)
+				return
+			}
+			http.SetCookie(w, &http.Cookie{Name: accessTokenName, Value: updatedAccess})
+			endPoint(w, r)
+			return
+		} else {
+			w.WriteHeader(http.StatusGone)
+			return
+		}
+	})
 }
 
 func isVerifiedAccess(r *http.Request) bool {
-	access, err := r.Cookie("access_token")
+	access, err := r.Cookie(accessTokenName)
 	if err != nil {
 		log.Println(err)
 		return false
 	}
-	/*if !isValidTime(access.Value, secretKeyAccess) {
+	if !isValidTime(access.Value, secretKeyAccess) {
 		log.Println("access token time is over")
 		return false
-	}*/
-	token, err := Parse(access.Value, secretKeyAccess)
+	}
+	token, err := parse(access.Value, secretKeyAccess)
 	if err != nil {
 		log.Println(err)
 		return false
@@ -111,15 +112,15 @@ func getUpdatedAccess(user models.User) (access string, err error) {
 }
 
 func isVerifiedRefresh(r *http.Request) bool {
-	refresh, err := r.Cookie("refresh_token")
+	refresh, err := r.Cookie(refreshTokenName)
 	if err != nil {
 		log.Println(err)
 		return false
 	}
-	/*if !isValidTime(refresh.Value, secretKeyRefresh) {
+	if !isValidTime(refresh.Value, secretKeyRefresh) {
 		log.Println("refresh token time is over")
 		return false
-	}*/
+	}
 	_, err = getUser(refresh.Value, secretKeyRefresh)
 	if err != nil {
 		return false
@@ -128,7 +129,7 @@ func isVerifiedRefresh(r *http.Request) bool {
 }
 
 func getUser(tokenString, secretKeyAccess string) (*models.User, error) {
-	token, err := Parse(tokenString, secretKeyAccess)
+	token, err := parse(tokenString, secretKeyAccess)
 	if err != nil {
 		log.Println(err)
 		return nil, err
@@ -151,25 +152,26 @@ func getUser(tokenString, secretKeyAccess string) (*models.User, error) {
 	return user, err
 }
 
-/*func isValidTime(tokenString, secretKey string) bool {
-	token, err := Parse(tokenString, secretKey)
+func isValidTime(tokenString, secretKey string) bool {
+	token, err := parse(tokenString, secretKey)
 	if err != nil {
 		log.Println(err)
 		return false
 	}
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		expString := fmt.Sprintf("%v", claims["exp"])
-		exp, err := strconv.ParseInt(expString, 0, 64)
+		expString := fmt.Sprintf("%f", claims["exp"])
+		exp, err := strconv.ParseFloat(expString, 64)
 		if err != nil {
 			fmt.Println(err)
 			return false
 		}
-		if exp < time.Now().Unix() {
-			return false
+		now := float64(time.Now().Unix())
+		if exp > now {
+			return true
 		}
 	}
 	return false
-}*/
+}
 
 func GetId(token *jwt.Token) (*jti, error) {
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
@@ -181,8 +183,7 @@ func GetId(token *jwt.Token) (*jti, error) {
 		}
 		return &jti, nil
 	}
-	err := errors.New("user not found") // remake
-	return nil, err
+	return nil, fmt.Errorf("user hasn't been found")
 }
 
 func GenerateAccess(user models.User) (tokenString string, err error) {
@@ -211,7 +212,7 @@ func GenerateRefresh(user models.User) (tokenString string, err error) {
 	claims := jwt.StandardClaims{
 		Issuer:    iss,
 		Id:        string(jti),
-		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+		ExpiresAt: time.Now().Add(time.Second * 10).Unix(),
 	}
 	rawToken := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
 	token, err := rawToken.SignedString([]byte(secretKeyRefresh))
